780b87495bb876dcbba94bef8c88ec550f4cb374
Roni <ronniiii@yahoo.com>
2022-10-12 16:57:04 +0100

    finished

--- 01_Day_JavaScript_Refresher/ex_l1-01.js
+++ 01_Day_JavaScript_Refresher/ex_l1-01.js
@@ -94,3 +94,75 @@ console.log(`12: ${itJoined} and ${last} are big IT companies \n`);
 // Exercise 13
 let itIndex = itCompanies.indexOf('banana') // 0
 itIndex != -1 ? console.log('13:', itIndex, '\n') : console.log('13: not found\n')
+
+// Exercise 14
+let dupli;
+for (const company of itCompanies) {
+    function search(str) {
+        for(var i = 0; i <= str.length; i++) {
+            for(var j = i+1; j <= str.length; j++) {
+                if(str[j] == str[i] && str[j] == 'o') {
+                    // console.log(`${str}`);
+                    dupli += `${str}, `;
+                    return false;
+                }
+            }
+        }
+    }
+    search(company);
+}
+var du = dupli.split(',');
+var du1 = dupli.split(',')[0];
+var due = du1.substr(9);
+dupli = [due, ...du.splice(1, 2)];
+console.log('14:', dupli.toString());
+
+// Exercise 15
+var sorted = itCompanies.sort();
+console.log('15:', sorted);
+
+// Exercise 16
+var reversed = itCompanies.reverse();
+console.log('16:', reversed);
+
+// Exercise 17
+sliceF3 = itCompanies.slice(0, 3)
+console.log('17:', sliceF3);
+
+// Exercise 18
+sliceL3 = itCompanies.slice(itCompanies.length - 3, itCompanies.length)
+console.log('18:', sliceL3);
+
+// Exercise 19
+var smid = getMid(itCompanies);
+if (typeof smid !== "object") {
+    var rm = itCompanies.slice(smid, smid + 1)
+    console.log('19:', rm);
+} else {
+    var [f, s] = smid;
+    var rm1 = itCompanies.slice(f, f + 1);
+    var rm2 = itCompanies.slice(s, s + 1);
+    console.log(`19: ${rm1}, ${rm2}`);
+}
+ 
+// Excercise 20
+itCompanies.shift();
+console.log('20:', itCompanies);
+
+// Exercise 21
+var spMid = getMid(itCompanies);
+if (typeof spMid == "object") {
+    var [mid1, mid2] = spMid;
+    itCompanies.splice(mid1, 2);
+} else{
+    itCompanies.splice(spMid, 1)
+}
+console.log('21:', itCompanies);
+
+// Exercise 22
+var pop = itCompanies.pop();
+console.log('22:', itCompanies);
+
+// Exercise 23
+var clearAll = itCompanies.splice(0);
+console.log('23:', itCompanies);
